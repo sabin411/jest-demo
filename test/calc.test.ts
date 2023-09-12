f7183a986052d1c8d985b8c126cf85a2ce5e4cdb
sabin411 <sherasabin411@gmail.com>
2023-09-12 23:08:48 +0545

    Test cases checl

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,17 +1,27 @@
 import { add, mul } from "../src/calc";
 
 describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
+  it("should return 15 for add(10, 5)", () => {
     expect(add(10, 5)).toBe(15);
   });
 
-  it("should return 5 for add(2,3)", () => {
+  it("should return 5 for add(2, 3)", () => {
     expect(add(2, 3)).toBe(5);
+  });
+
+  it("should return 6 for mul(2, 3)", () => {
+    expect(mul(2, 3)).toBe(6);
   });
 });
 
 describe("test mul function", () => {
-  it("should return 15 for mul(3,5)", () => {
+  it("should return 15 for mul(3, 5)", () => {
     expect(mul(3, 5)).toBe(15);
+  });
+});
+
+describe("Sabin Testing", () => {
+  it("Checking if the test passes", () => {
+    expect(mul(4, 5)).toBe(20);
   });
 });
